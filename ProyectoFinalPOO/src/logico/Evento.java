60ace5b9b1018f7d65561052f05d8fa26e1918e6
ilonk <ilonk@LAPTOP-0QFRVB2T>
2024-11-25 18:40:38 -0400

    Crear clae evento y Recurso

--- ProyectoFinalPOO/src/logico/Evento.java
+++ ProyectoFinalPOO/src/logico/Evento.java
@@ -1,5 +1,59 @@
 package logico;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Evento {
 
+	private String id;
+	private String titulo;
+	private String tipo;
+	private Date fecha;
+	private ArrayList<Comision> comisiones;
+	private ArrayList<TrabajoCientifico> trabajosCientificos;
+	private ArrayList<Recurso> recursos;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public ArrayList<Comision> getComisiones() {
+		return comisiones;
+	}
+	public void setComisiones(ArrayList<Comision> comisiones) {
+		this.comisiones = comisiones;
+	}
+	public ArrayList<TrabajoCientifico> getTrabajosCientificos() {
+		return trabajosCientificos;
+	}
+	public void setTrabajosCientificos(ArrayList<TrabajoCientifico> trabajosCientificos) {
+		this.trabajosCientificos = trabajosCientificos;
+	}
+	public ArrayList<Recurso> getRecursos() {
+		return recursos;
+	}
+	public void setRecursos(ArrayList<Recurso> recursos) {
+		this.recursos = recursos;
+	}
 }
