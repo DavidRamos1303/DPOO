f8a6e359d5c41d8f5318d7f0295569398ce0552b
ilonk <ilonk@LAPTOP-0QFRVB2T>
2024-11-26 22:48:28 -0400

    ...

--- ProyectoFinalPOO/src/visual/ListPersona.java
+++ ProyectoFinalPOO/src/visual/ListPersona.java
@@ -10,10 +10,17 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
+import javax.swing.JTable;
 
 public class ListPersona extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private JTable table_1;
 
 	/**
 	 * Launch the application.
@@ -32,6 +39,7 @@ public class ListPersona extends JDialog {
 	 * Create the dialog.
 	 */
 	public ListPersona() {
+		setTitle("Listar Participantes y Jurados");
 		setBounds(100, 100, 669, 450);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(255, 255, 0));
@@ -45,13 +53,31 @@ public class ListPersona extends JDialog {
 			panel.setLayout(null);
 			{
 				JPanel panel_1 = new JPanel();
-				panel_1.setBounds(33, 11, 231, 295);
+				panel_1.setBackground(new Color(0, 102, 255));
+				panel_1.setBorder(new TitledBorder(null, "Participantes", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+				panel_1.setBounds(10, 11, 296, 350);
 				panel.add(panel_1);
+				panel_1.setLayout(new BorderLayout(0, 0));
+				
+				JScrollPane scrollPane = new JScrollPane();
+				panel_1.add(scrollPane, BorderLayout.CENTER);
+				
+				table = new JTable();
+				scrollPane.setViewportView(table);
 			}
 			{
 				JPanel panel_1 = new JPanel();
-				panel_1.setBounds(375, 11, 231, 295);
+				panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Jurados", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+				panel_1.setBackground(new Color(0, 102, 255));
+				panel_1.setBounds(341, 11, 296, 350);
 				panel.add(panel_1);
+				panel_1.setLayout(new BorderLayout(0, 0));
+				
+				JScrollPane scrollPane = new JScrollPane();
+				panel_1.add(scrollPane, BorderLayout.CENTER);
+				
+				table_1 = new JTable();
+				scrollPane.setViewportView(table_1);
 			}
 		}
 		{
@@ -60,11 +86,10 @@ public class ListPersona extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton cancelButton = new JButton("Cancel");
+				JButton cancelButton = new JButton("Cancelar");
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-
 }
